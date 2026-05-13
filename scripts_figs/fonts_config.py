fcaec839e7e0457b7c59eb57ae0b669950461506
luciagutierrezg <lucgut03@ucm.es>
2026-05-13 15:29:15 +0200

    update all figures

--- scripts_figs/fonts_config.py
+++ scripts_figs/fonts_config.py
@@ -4,10 +4,10 @@ import matplotlib.colors as mcolors
 import numpy as np
 
 def set_computer_modern():
-    font_path = "/home/luciagu/tools/fonts/cmunrm.ttf"
-    font_manager.fontManager.addfont(font_path)  
-    font_prop = font_manager.FontProperties(fname=font_path)
-    rcParams['font.family'] = font_prop.get_name()
+    # font_path = "/home/luciagu/tools/fonts/cmunrm.ttf"
+    # font_manager.fontManager.addfont(font_path)  
+    # font_prop = font_manager.FontProperties(fname=font_path)
+    # rcParams['font.family'] = font_prop.get_name()
     rcParams['font.size'] = 12
 
 def truncate_colormap(cmap, minval, maxval):
